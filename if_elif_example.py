335733f3db5ddbcaed41f01e31e30c0951c06d2b
SOPHINA <mac@SOPHIE.local>
2019-07-02 13:41:47 +0100

    Day 1

---
+++ if_elif_example.py
@@ -0,0 +1,11 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not ALice,  kiddo.')
+elif age > 2000:
+    print ('Nice trym Alice is not an undead knight queen')
+elif age > 100:
+    print('You are not Alice, grannie.')
+    
