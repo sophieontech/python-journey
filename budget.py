cecc2cb923f3d8e0439cdd42091ce9e7769c90cb
Sophina Kio-Lawson <26139563+sophieontech@users.noreply.github.com>
2021-04-18 11:12:07 +0100

    This is a budgeting app

---
+++ budget.py
@@ -0,0 +1,49 @@
+class myBudget:
+    """
+    This is Sophie's Budgeting App
+    """
+    pass
+    def __init__(self, category, balance):
+        self.category = category
+        self.balance = 0
+
+
+    def deposit(self,depositAmount):
+        try:
+            if depositAmount <= 0:
+                return "You have entered an invalid amount"
+            else:
+                self.balance += depositAmount
+                print("You have deposited {} into your {} ".format(depositAmount, self.category))
+                print("Your {} is now {}".format(self.category, self.balance))
+        except:
+            return ("Invalid Input")
+
+    def withdrawal(self,withdrawAmount):
+        if withdrawAmount <= self.balance:
+            self.balance -= withdrawAmount
+            print("You have just withdrawn {} from your {}".format(withdrawAmount,self.category))
+            return self.categoryBalance()
+        else:
+            return "Your balance is insufficient"
+
+    def transfer(self,transferAmount):
+        print("You have just transfered {} from your {}".format(transferAmount, self.category))
+        return transferAmount
+
+    def categoryBalance(self):
+        return "The current balance left in the {} is {}".format(self.category, self.balance)
+
+
+clothing = myBudget('shopping', 10000)
+food = myBudget('food',5000)
+entertainment = myBudget('entertainment',5000)
+
+print(clothing.deposit(50000))
+print(clothing.withdrawal(20000))
+print(entertainment.deposit(50000))
+print(entertainment.withdrawal(5000))
+print(entertainment.categoryBalance())
+
+
+print(type(myBudget))
